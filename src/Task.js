36567cb4a349cdef39c3cd37a75e67cd79ef2e93
VeronicaMartinez2024 <veronica.martinez2024@hilasal.superate.org.sv>
2023-10-27 23:00:15 -0600

    :lipstick: Versión final proyecto

--- src/Task.js
+++ src/Task.js
@@ -1,22 +1,24 @@
 class Task {
     constructor({
-    name,
-    details,
-    startTime,
-    endTime,
-    status
-    }){
-    this.name = name;
-    this.details = details;
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.status = status;
+        name,
+        details = "Detalles no disponibles",
+        startTime,
+        endTime,
+        status
+    }) {
+        this.name = name;
+        this.details = details;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
     }
+
     getHtml() {
+        
         return `
-            <li class="bg-white p-4 rounded shadow">
-                <h3 class="text-lg font-semibold">${this.name}</h3>
-                <p>${this.description}</p>
+            <li draggable="true">
+                <h3>${this.name}</h3>
+                <p>${this.details}</p>
                 <p>Inicio: ${this.startTime}</p>
                 <p>Fin: ${this.endTime}</p>
                 <p>Estado: ${this.status}</p>
@@ -24,14 +26,6 @@ class Task {
         `;
     }
 }
-
-const task = new Task({
-    name : name,
-    details: details,
-    startTime: startTime,
-    endTime: endTime,
-    status: status
-});
 
 class TaskManager {
     constructor() {
@@ -45,20 +39,10 @@ class TaskManager {
     addTask(task) {
         this.tasks[task.status].push(task);
     }
-
-    removeTask(task) {
-        const index = this.tasks[task.status].indexOf(task);
-        if (index !== -1) {
-            this.tasks[task.status].splice(index, 1);
-        }
-    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     const taskManager = new TaskManager();
-    const todoList = document.getElementById("todoList");
-    const inProgressList = document.getElementById("inProgressList");
-    const doneList = document.getElementById("doneList");
     const addTaskButton = document.getElementById("addTaskButton");
 
     addTaskButton.addEventListener("click", function () {
@@ -68,14 +52,20 @@ document.addEventListener("DOMContentLoaded", function () {
         const endTime = document.getElementById("endTime").value;
         const taskStatus = document.getElementById("taskStatus").value;
 
-        const task = new Task(taskName, taskDescription, startTime, endTime, taskStatus);
+        const task = new Task({
+            name: taskName,
+            details: taskDescription,
+            startTime,
+            endTime,
+            status: taskStatus
+        });
 
-        // Add the task to task manager
+        // Add the task to the task manager
         taskManager.addTask(task);
 
         // Add task to HTML
-        const taskList = document.getElementById(`${taskStatus}List`);
-        taskList.innerHTML += task.getHtml();
+        const taskList = document.getElementById(task.status + "List");
+        taskList.insertAdjacentHTML("beforeend", task.getHtml());
 
         // Clear fields after adding tasks
         document.getElementById("taskName").value = "";
@@ -84,4 +74,46 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("endTime").value = "";
         document.getElementById("taskStatus").value = "pendiente";
     });
+    document.addEventListener("DOMContentLoaded", function () {
+        const tasks = document.querySelectorAll(".draggable");
+        const columns = document.querySelectorAll(".droppable");
+    
+        let draggedItem = null;
+    
+        tasks.forEach((task) => {
+            task.addEventListener("dragstart", () => {
+                draggedItem = task;
+                setTimeout(() => {
+                    task.style.display = "none";
+                }, 0);
+            });
+    
+            task.addEventListener("dragend", () => {
+                setTimeout(() => {
+                    draggedItem.style.display = "block";
+                    draggedItem = null;
+                }, 0);
+            });
+        });
+    
+        columns.forEach((column) => {
+            column.addEventListener("dragover", (e) => {
+                e.preventDefault();
+            });
+    
+            column.addEventListener("dragenter", (e) => {
+                e.preventDefault();
+                column.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+            });
+    
+            column.addEventListener("dragleave", () => {
+                column.style.backgroundColor = "transparent";
+            });
+    
+            column.addEventListener("drop", () => {
+                column.appendChild(draggedItem);
+                column.style.backgroundColor = "transparent";
+            });
+        });
+    });    
 });
